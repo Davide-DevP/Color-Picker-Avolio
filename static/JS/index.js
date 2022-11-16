14332cebc84ad7d8e3db0dfa66131bb71d5f7b01
Davide-DevP <80261869+Davide-DevP@users.noreply.github.com>
2022-11-16 12:49:16 +0100

    Update index.js

--- static/JS/index.js
+++ static/JS/index.js
@@ -2,12 +2,4 @@ function changeBG(button, color){
     document.getElementById("bgButtonPressed").id = 'bgButton';
     button.id = 'bgButtonPressed';
     document.body.style.backgroundColor = color;
-    color = hexToRgb(color);
-    for(var i=0; i<buttons.length;i++){
-        if(checkColor(buttons[i].style.backgroundColor,color)){
-            buttons[i].style.border = "2px solid black";
-        }else if(buttons[i].style.border == "2px solid black"){
-            buttons[i].style.border = "none";
-        }
-    }
 }
