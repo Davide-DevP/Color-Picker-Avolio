4b466c2ef607dd74f5fb3099412f0ea495400788
Davide-DevP <80261869+Davide-DevP@users.noreply.github.com>
2022-11-16 12:45:16 +0100

    Fixed code

--- static/JS/index.js
+++ static/JS/index.js
@@ -1,39 +1,6 @@
-function hexToRgb(hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : null;
-}
-
-function checkColor(colorToCheck, checkValue){
-    var rgb = colorToCheck.match(/\d+/g);
-    var check = true;
-    for(var i in rgb) {
-        switch(i){
-            case '0':
-                if(rgb[0]!=checkValue.r) {
-                    check = false;
-                }
-                break;
-            case '1':
-                if(rgb[1]!=checkValue.g) {
-                    check = false;
-                }
-                break;
-            case '2':
-                if(rgb[2]!=checkValue.b) {
-                    check = false;
-                }
-                break;
-        }
-    }
-    return check;
-}
-
-let buttons = document.getElementsByClassName("bgButton");
-function changeBG(color){
+function changeBG(button, color){
+    document.getElementById("bgButtonPressed").id = 'bgButton';
+    button.id = 'bgButtonPressed';
     document.body.style.backgroundColor = color;
     color = hexToRgb(color);
     for(var i=0; i<buttons.length;i++){
